bc967853a1663e42888a636b0b08670cc5b5244c
Pavi <mhmtozyurt97@gmail.com>
2021-04-11 07:24:52 +0300

    hotfix?

--- bot.js
+++ bot.js
@@ -8,13 +8,12 @@ client.on('ready', () => {
   console.log(`Sunucumuza ${client.user.tag} giris yapti!`);
 });
 client.on('message', msg => {
-  if (msg.content === '!komutlar') {
+  if (!msg.content.startsWith(prefix)) return;
+
+  if (msg.content.startsWith(prefix + "komutlar")) {
     msg.channel.sendMessage('Sadece tek bir komutum var :( - !abdest ');
-  }
-  if (!msg.content.startsWith(prefix)) {
-    return;
-  }
-  if (msg.content.toLowerCase() === prefix + 'abdest') {
+  } else
+  if (msg.content.startsWith(prefix + "abdest")) {
     msg.channel.sendMessage('Galiba birilerinin abdest almaya ihtiyaci var o zaman: https://quickabdest.com/');
   }
 });
