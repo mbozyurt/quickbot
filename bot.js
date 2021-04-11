7db0af776249a7636b93307d88bc676b089b7193
Pavi <mhmtozyurt97@gmail.com>
2021-04-11 07:36:09 +0300

    test

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('message', msg => {
   if (!msg.content.startsWith(prefix)) return;
 
   if (msg.content.startsWith(prefix + "komutlar")) {
-    msg.channel.sendMessage('Sadece tek bir komutum var :( - !abdest ');
+    msg.channel.send('Sadece tek bir komutum var :( - !abdest ');
   } else
   if (msg.content.startsWith(prefix + "abdest")) {
-    msg.channel.sendMessage('Galiba birilerinin abdest almaya ihtiyaci var o zaman: https://quickabdest.com/');
+    msg.channel.send('Galiba birilerinin abdest almaya ihtiyaci var o zaman: https://quickabdest.com/');
   }
 });
 
