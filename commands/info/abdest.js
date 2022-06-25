9d10ac0f507cb407e066cf0fbbd76f5658fc5766
Berk <mhmtozyurt97@gmail.com>
2022-06-25 19:24:13 +0300

    update

---
+++ commands/info/abdest.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "abdest",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async({client, message, args}) => {
+        message.reply("Abdest almak için tıkla! https://quickabdest.com")
+    }
+}
