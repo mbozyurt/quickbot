fead88c0a73ffc467f2dc5823ab1b9db1051fd12
Berk <mhmtozyurt97@gmail.com>
2022-07-04 02:38:19 +0300

    this command first init

---
+++ commands/info/this.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "this",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async({client, message, args}) => {
+        message.reply("https://tenor.com/bi00K.gif")
+    }
+}
