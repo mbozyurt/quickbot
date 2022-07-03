47592d0ca46cb50ced478303a278788c7997d329
Berk <mhmtozyurt97@gmail.com>
2022-07-04 02:25:24 +0300

    hakan command

---
+++ commands/info/hakan.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "abdest",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async({client, message, args}) => {
+        message.reply("https://tenor.com/bPOmB.gif")
+    }
+}
