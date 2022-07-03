43213a0bf479ea428e0bd9272054743364dcaee6
Berk <mhmtozyurt97@gmail.com>
2022-07-04 02:34:12 +0300

    hakan fix

--- commands/info/hakan.js
+++ commands/info/hakan.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "abdest",
+    name: "hakan",
     category: "info",
     permissions: [],
     devOnly: false,
--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        message.reply("Pongv2")
+        message.reply("Pong")
     }
 }
