2757894d7c7216ce52f5f077458e4bcd6ff15ae3
Berk <mhmtozyurt97@gmail.com>
2022-08-03 17:00:05 +0300

    activity

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const Discord = require("discord.js")
-require("dotenv").config()
+const Discord = require("discord.js");
+require("dotenv").config();
 
 // const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
-    ]
-})
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
+});
 
 let bot = {
-    client,
-    prefix: "!",
-    owners: ["145629655171465216"]
-}
+  client,
+  prefix: "!",
+  owners: ["145629655171465216"],
+};
 
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
-client.slashcommands = new Discord.Collection()
-client.buttons = new Discord.Collection()
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+client.slashcommands = new Discord.Collection();
+client.buttons = new Discord.Collection();
 
-client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
-client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) =>
+  require("./handlers/commands")(bot, reload);
+client.loadSlashCommands = (bot, reload) =>
+  require("./handlers/slashcommands")(bot, reload);
+client.loadButtons = (bot, reload) =>
+  require("./handlers/buttons")(bot, reload);
 
-client.loadEvents(bot, false)
-client.loadCommands(bot, false)
-client.loadSlashCommands(bot, false)
-client.loadButtons(bot, false)
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+client.loadSlashCommands(bot, false);
+client.loadButtons(bot, false);
 
-
-module.exports = bot
-
-client.login(process.env.TOKEN)
+module.exports = bot;
+client.on("ready", () => {
+  client.user.setActivity("!abdest");
+});
+client.login(process.env.TOKEN);
